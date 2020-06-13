741ce26de6f6aa2d3ebe7a06b6dc548efe7cc90f
Daniel Trabontea <danielmbotea@gmail.com>
2020-06-13 19:21:20 +0300

    Updates

---
+++ precache-manifest.b9afa2bcf1c5a7875d90326cacee67d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98cb0c4633cfe253f01bb2d9ed0c4b98",
+    "url": "/covid-19-world/index.html"
+  },
+  {
+    "revision": "082fc669e6c53a4185f7",
+    "url": "/covid-19-world/static/css/main.d89759f7.chunk.css"
+  },
+  {
+    "revision": "f1e5e853806a4c00a431",
+    "url": "/covid-19-world/static/js/2.bd8de6e2.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/covid-19-world/static/js/2.bd8de6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082fc669e6c53a4185f7",
+    "url": "/covid-19-world/static/js/main.c6d56ef4.chunk.js"
+  },
+  {
+    "revision": "5ed97ee51992b2245efd",
+    "url": "/covid-19-world/static/js/runtime-main.60108701.js"
+  },
+  {
+    "revision": "5603b6bad7f7570c7e018e516700005a",
+    "url": "/covid-19-world/static/media/hands-and-gestures.5603b6ba.svg"
+  },
+  {
+    "revision": "5799cd24553da629ce5894874ff6382c",
+    "url": "/covid-19-world/static/media/sort.5799cd24.svg"
+  }
+]);
